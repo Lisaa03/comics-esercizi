80d114fda80150aef694ed5520b542a7d8be2a68
Lisaa03 <151755418+Lisaa03@users.noreply.github.com>
2023-11-27 17:46:46 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//es1
+//int i = Convert.ToInt32(Console.ReadLine()); // questa linea riceve un numero
+//int j = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine($"{i}");
+//Console.WriteLine($"{j}");
+
+//int k = j;
+//Console.WriteLine($"{k}");
+//Console.WriteLine($"{i}");
+////es2
+
+//int Max = int.MaxValue; //
+//Console.WriteLine($"{Max}");
+
+//int E = Max + 1;
+//Console.WriteLine($"{E}");
+//es3 π * r^2
+//Console.WriteLine("Metti un numero per il raggio del cerchio");
+//int r = Convert.ToInt32(Console.ReadLine());
+//double a = Math.PI * (r^2);
+//Console.WriteLine($"L'area del cerchio è {a}");
+//es4
+Console.WriteLine("metti 4 numeri");
+int uno = Convert.ToInt32(Console.ReadLine());
+int due = Convert.ToInt32(Console.ReadLine());
+int tre = Convert.ToInt32(Console.ReadLine());
+int quattro = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+
+
+
