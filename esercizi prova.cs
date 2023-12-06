2066ccc6d7c3e2c849343670df59300a8ded3045
Lisaa03 <151755418+Lisaa03@users.noreply.github.com>
2023-12-06 18:00:54 +0100

    Add files via upload

---
+++ esercizi prova.cs
@@ -0,0 +1,71 @@
+﻿//See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//ex 3
+//int numero1 = Convert.ToInt32(Console.ReadLine());
+//int numero = 1;
+//int numero2 = 1;
+//while (numero >= 1)
+//{
+//    Console.WriteLine(numero * numero2);
+//    numero++;
+//}
+
+//ex 00
+//int voto = Convert.ToInt32(Console.ReadLine());
+//if (voto < 0 || voto > 100)
+
+//{
+//    Console.WriteLine("il voto invalido");
+//}
+//else if (voto >= 90)
+//{
+//    Console.WriteLine("eccellente");
+//}
+//else if (voto >= 70)
+//{
+//    Console.WriteLine("buono");
+//}
+//else if (voto >= 50)
+//{
+//    Console.WriteLine("sufficiente");
+//}
+//else
+//{
+//    Console.WriteLine("insufficiente");
+//}
+////int numero = 1;//ex01
+////int numero = Convert.ToInt32(Console.ReadLine());
+
+//for (int numero = 1; numero <= 10; numero++)
+//{
+//    Console.WriteLine( numero * 7);
+//}
+//ex02
+
+//int numero_da_sommare = 2;
+//int risultato = 0;
+
+//while (numero_da_sommare <= 20)
+//{
+//    if (numero_da_sommare % 2 == 0)
+//        risultato += numero_da_sommare;
+    
+//   numero_da_sommare++;
+//}
+////ex 004
+////for (int numero = 4; numero   )
+
+//// primi 10 divisibili per 3
+int numeroprimo = 3;
+    int numerodue = 0;
+while (numeroprimo <= 30)
+{
+    if (numerodue % 3 == 0)
+        numerodue += numeroprimo;
+            numeroprimo++;
+}
+
+
+
+
+
