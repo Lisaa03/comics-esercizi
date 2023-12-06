bed37d703732afa4df3573159bbbe1276cd5f08f
Lisaa03 <151755418+Lisaa03@users.noreply.github.com>
2023-12-06 14:18:39 +0100

    Add files via upload

---
+++ esercizi seconda lezione.cs
@@ -0,0 +1,43 @@
+﻿
+////ex 3
+//int numero1 = Convert.ToInt32(Console.ReadLine());
+//int numero = 1;
+//int numero2 = 1;
+//while (numero >= 1)
+//{ 
+//Console.WriteLine(numero * numero2);
+// numero++;
+//}
+
+//ex 00
+//int voto = Convert.ToInt32(Console.ReadLine());
+//if (voto >= 0 || voto <= 100);
+//{
+//    Console.WriteLine("il voto e vero");
+//}
+//else if (voto >= 90)
+//{
+//    Console.WriteLine("eccellente");
+//}
+//else if (voto >= 70)
+//{
+//    Console.WriteLine("buono");
+//}
+//else if (voto >= 50) ;
+//{
+//    Console.WriteLine("sufficiente");
+//}
+//else
+//{
+//   Console.WriteLine("insufficiente");
+//}
+//ex01
+//int numero = Convert.ToInt32(Console.ReadLine());
+//int numero = 1;
+//for (int numero = 1; numero <= 10; numero++) ;
+//{
+//    Console.WriteLine(numero * 7);
+//}
+//ex02
+
+
