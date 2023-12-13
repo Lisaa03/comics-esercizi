7a38b2463b611880ada84a4f22fa16f7e7cb7cf5
Lisaa03 <151755418+Lisaa03@users.noreply.github.com>
2023-12-13 18:12:12 +0100

    ho aggiunto l progetto

--- Program.cs
+++ Program.cs
@@ -1,37 +1,74 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//es1
-//int i = Convert.ToInt32(Console.ReadLine()); // questa linea riceve un numero
-//int j = Convert.ToInt32(Console.ReadLine());
-//Console.WriteLine($"{i}");
-//Console.WriteLine($"{j}");
+//ex 00
 
-//int k = j;
-//Console.WriteLine($"{k}");
-//Console.WriteLine($"{i}");
-////es2
+//internal class Program
+//{
+//    static void Main(string[] args)
+//    {
+//    float r = 1;
+//    calcolaarea(r);
+//    float x = 3;
+//    calcolaarea(x);
+//    }
+//public static void calcolaarea(float raggio)
+//{
+//    Console.WriteLine(raggio * 2 * Math.PI);
+//}
+//}
 
-//int Max = int.MaxValue; //
-//Console.WriteLine($"{Max}");
+//ex01
+//internal class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        int uno = 2;
+//        int due = 4;
+//        trovamax(uno, due);
+//    }
 
-//int E = Max + 1;
-//Console.WriteLine($"{E}");
-//es3 π * r^2
-//Console.WriteLine("Metti un numero per il raggio del cerchio");
-//int r = Convert.ToInt32(Console.ReadLine());
-//double a = Math.PI * (r^2);
-//Console.WriteLine($"L'area del cerchio è {a}");
-//es4
-Console.WriteLine("metti 4 numeri");
-int uno = Convert.ToInt32(Console.ReadLine());
-int due = Convert.ToInt32(Console.ReadLine());
-int tre = Convert.ToInt32(Console.ReadLine());
-int quattro = Convert.ToInt32(Console.ReadLine());
+//    public static void trovamax(int uno, int due)
+//    {
+//        if (uno < due)
+//        {
+//            Console.WriteLine("il valore massimo e 4");
+//        }
+//        else
+//        {
+//            Console.WriteLine("il valore minimo e 2");
+//        }
+//    }
+
+//}
+//ex02
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> spesa = new List<string> { "mele", "latte", "zucchero"};
+
+    }
 
 
-
-
-
-
-
-
+    static void rimuovi(List<string> spesa, string oggetto)
+    {
+        spesa.Remove(oggetto);
+    }
+    static void aggiungi(List<string> spesa, string oggetto)
+    {
+        spesa.Add(oggetto);
+    }
+    static void visualizza(List<string> spesa)
+    {
+        foreach (string elementos in spesa)
+        {
+            Console.WriteLine(elementos);
+        }
+    }
+}
